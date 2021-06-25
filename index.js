01823bc8b3ccec4b4ce1893572012c32278060bd
Renhard Julindra <julindrarenhard7@gmail.com>
2021-06-25 14:20:17 +0700

    add feature register

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 const PORT = 3000;
 
+app.post('/register', (req, res) => {
+  const { email, password } = req.body;
+
+  res.status(201).json({ message: 'success', email });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
