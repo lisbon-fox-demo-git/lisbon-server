5b2e6af5608ccb17e4a66128b8806d0db0911093
Renhard Julindra <julindrarenhard7@gmail.com>
2021-06-25 14:10:40 +0700

    add login routing

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const app = express();
 
 const PORT = 3000;
 
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({
+    where: { email }
+  }).then(user => {
+    res.status(200).json({ access_token: email });
+  }).catch(err => {
+    console.log(err);
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
